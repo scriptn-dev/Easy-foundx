38b3382cb68a2994aaace6c1455b4aac3e069909
jhonatanborg <jhonatanborgesdj@gmail.com>
2020-08-22 03:39:40 -0400

    new configs

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 import { Promise } from "es6-promise";
-import router from "@/router";
-import store from "@/store";
 
 const http = axios.create({
   baseURL: "https://api.easyfundx.com/",
@@ -11,5 +9,16 @@ const http = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+http.interceptors.response.use(
+  function(response) {
+    return response;
+  },
+  function(error) {
+    if (error.response.status === 401) {
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default http;
