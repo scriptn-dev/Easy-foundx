09aed7f1e9ec116ee5beaedbcc08cd4c39340237
jhonatanborg <jhonatanborgesdj@gmail.com>
2020-08-22 03:35:15 -0400

    new configs

--- src/services/config.js
+++ src/services/config.js
@@ -12,38 +12,4 @@ const http = axios.create({
   },
 });
 
-http.interceptors.response.use(
-  function(response) {
-    return response;
-  },
-  function(error) {
-    if (error.response.status === 401) {
-      if (error.config.url === "auth/refresh") {
-        store.commit("LOGOUT_USER");
-        localStorage.clear();
-        router.push({ name: "Login" });
-      } else {
-        http
-          .get("auth/refresh")
-          .then((resp) => {
-            const config = error.config;
-            config.headers[
-              "Authorization"
-            ] = `Bearer ${resp.data.access_token}`;
-            localStorage.setItem("token", resp.data.access_token);
-
-            const retryOriginal = new Promise((resolve) => {
-              resolve(http.request(error.config));
-            });
-            return retryOriginal;
-          })
-          .catch((err) => {
-            console.log("ERROR INTERCEPTOR", err);
-          });
-      }
-    }
-    return Promise.reject(error);
-  }
-);
-
 export default http;
